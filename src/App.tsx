839d6882e775e4cff75750252e613f0616d47011
Jay Feldman <jay.feldman03@gmail.com>
2023-05-08 08:41:23 -0400

    ensures the image image is distinct from the old one

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,12 @@ const baseUrl = 'https://acroyoga757.com';
 const imagesPath = `${baseUrl}/acroimages`;
 const reloadButtonUrl = 'ClickForNewPose.png';
 
-// Takes 4 => 004, 34 => 034, 944 => 944
-const fillIndexToThreeDigits = (index: number): string => {
-  const stringified = index.toString();
-  if (stringified.length > 2) {
-    return stringified;
-  } else if (stringified.length === 2) {
-    return `0${stringified}`;
-  }
-  return `00${stringified}`;
-};
-
 const poses = Array(76)
   .fill('')
-  .map((_, index) => `${imagesPath}/image-${fillIndexToThreeDigits(index)}.jpg`);
+  .map((_, index) => {
+    const imageNumber = index.toString().padStart(3, '0'); // Converts 4 => 004, 34 => 034 etc
+    return `${imagesPath}/image-${imageNumber}.jpg`;
+  });
 
 const getRandomPose = (): string => {
   return poses[Math.floor(Math.random() * poses.length)];
@@ -26,6 +18,15 @@ const getRandomPose = (): string => {
 
 function App() {
   const [activePose, setActivePose] = useState(getRandomPose());
+
+  const setNewPose = () => {
+    let newPose = getRandomPose();
+    while (newPose === activePose) {
+      // Ensures that the new image doesn't happen to be the same as the old image
+      newPose = getRandomPose();
+    }
+    setActivePose(newPose);
+  };
 
   return (
     <div className="App">
@@ -44,7 +45,7 @@ function App() {
         height="50"
         style={{ marginTop: 20, cursor: 'pointer' }}
         alt="Reload pose"
-        onClick={() => setActivePose(getRandomPose())}
+        onClick={setNewPose}
       />
       <br />
       <img src={activePose} alt="Pose" style={{ marginTop: 20 }} />
